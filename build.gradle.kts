db4fbe15eeb659e397b884fbdde68481976bc69b
Szymon <shateq21@gmail.com>
2022-11-29 21:34:34 +0100

    1.19.3-pre3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
+import com.matthewprenger.cursegradle.CurseProject
+import com.matthewprenger.cursegradle.CurseRelation
+import com.matthewprenger.cursegradle.Options
+
 plugins {
-    id("fabric-loom") version "0.12-SNAPSHOT"
+    id("fabric-loom") version "1.0-SNAPSHOT"
     id("com.modrinth.minotaur") version "2.+"
+    id("com.matthewprenger.cursegradle") version "1.4.0"
+
     kotlin("jvm") version "1.7.0"
 }
 fun p(key: String): String = project.extra.get(key) as String
 
-version = "1.1.1"
+version = "1.2.0"
 group = "shateq.mods" //no maven publish
 base.archivesName.set("disconnect-keybind-$version-fabric-${p("mc")}")
 description = "Bind yourself a button to Disconnect!"
@@ -13,8 +19,8 @@ description = "Bind yourself a button to Disconnect!"
 dependencies {
     minecraft("com.mojang:minecraft:${p("mc")}")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:${p("loader")}")
 
+    modImplementation("net.fabricmc:fabric-loader:${p("loader")}")
     modImplementation("net.fabricmc:fabric-language-kotlin:${p("kotlin_version")}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${p("fapi_version")}")
 }
@@ -36,10 +42,7 @@ tasks {
     processResources {
         filteringCharset = "UTF-8"
         filesMatching("fabric.mod.json") {
-            expand(
-                "version" to version,
-                "description" to description
-            )
+            expand("version" to version)
         }
     }
 }
@@ -47,14 +50,42 @@ tasks {
 modrinth {
     token.set(System.getenv("MODRINTH_TOKEN")) // This is the default. Remember to have the MODRINTH_TOKEN environment variable set or else this will fail, or set it to whatever you want - just make sure it stays private!
     projectId.set("disconnect")
+    versionName.set("Disconnect Keybind $version for MC ${p("mc")}")
     versionNumber.set(version.toString())
     versionType.set("release")
 
-    uploadFile.set(tasks.remapJar.name)
-    gameVersions.addAll("1.19", "1.19.1", "1.19.2")
+    uploadFile.set(tasks["remapJar"])
+    gameVersions.addAll("1.19.3-pre2", "1.19.3-pre3")
     dependencies {
         // scope.type : can be `required`, `optional`, `incompatible`, or `embedded`
         required.project("fabric-api")
         required.project("fabric-language-kotlin")
     }
+}
+
+//TODO is failing
+curseforge {
+    apiKey = System.getenv("CURSE")
+    options(closureOf<Options> {
+        forgeGradleIntegration = false
+        javaIntegration = true
+    })
+    project(closureOf<CurseProject> {
+        id = "disconnect"
+        releaseType = "release"
+        changelog = "Support 1.19.3-pre3"
+
+        relations(closureOf<CurseRelation> {
+            requiredDependency("fabric-api")
+            requiredDependency("fabric-language-kotlin")
+        })
+
+        addGameVersion("Fabric")
+        addGameVersion("1.19.3")
+        addGameVersion("Java 17")
+        mainArtifact(tasks.remapJar.get().archiveFile.get().asFile)
+        afterEvaluate {
+            uploadTask.dependsOn(tasks.remapJar)
+        }
+    })
 }
